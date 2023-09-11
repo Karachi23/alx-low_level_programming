4386d0b55fe5156a55e153e73de3bd24a988ccf3
Karachi23 <okerekeyvonne@yahoo.com>
2023-09-11 15:23:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ /**
+ *main - A program that prints a line using the printf f
+unction
+ *Return: 0(Success)
+ */
+int (void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
